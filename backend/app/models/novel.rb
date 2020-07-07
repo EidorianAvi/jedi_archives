86bf8977fc5e36a2f85516aee61d3935a73d04ca
EidorianAvi <reneavila1993@gmail.com>
2020-07-07 15:47:11 -0600

    Start incorporating cloudinary

--- backend/app/models/novel.rb
+++ backend/app/models/novel.rb
@@ -1,4 +1,11 @@
 class Novel < ApplicationRecord
     has_many :novel_collections
     has_many :users, through: :novel_collections
+    validates :title, :author, :release_date, :cover_art, :summary, presence: true
+    
+    has_one_attached :cover_art
+
+    def get_image_url
+        url_for(self.cover_art)
+    end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_165100) do
+ActiveRecord::Schema.define(version: 2020_07_07_213638) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "graphic_novel_collections", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -57,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_165100) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "graphic_novel_collections", "graphic_novels"
   add_foreign_key "graphic_novel_collections", "users"
   add_foreign_key "novel_collections", "novels"
