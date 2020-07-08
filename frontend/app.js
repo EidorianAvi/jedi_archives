9d40c008551c5530bf4ba22f41d33f3184c3f670
EidorianAvi <reneavila1993@gmail.com>
2020-07-08 13:10:42 -0600

    Add graphic novel form and handle its post

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,37 @@
-const novelForm = document.querySelector('#novel-form');
+const createUserForm = document.querySelector("#create-user-form");
 const loginForm = document.querySelector('#login-form');
+const novelForm = document.querySelector('#novel-form');
+const graphicNovelForm = document.querySelector("#graphic-novel-form");
 const logoutButton = document.querySelector('#logout-button');
 const novelAPI = "http://localhost:3000/novels";
 const graphicNovelAPI = "http://localhost:3000/graphic_novels";
+const userAPI = "http://localhost:3000/users";
 
 
-loginForm.addEventListener('submit', handleLoginSubmit)
+createUserForm.addEventListener('submit', handleCreateSubmit);
+
+function handleCreateSubmit(event){
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    handleCreateData(formData);
+}
+
+function handleCreateData(formData){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: formData.get('username'),
+            password: formData.get('password')
+        })
+    })
+    .then(response => response.json())
+    .then(console.log)
+}
+
+loginForm.addEventListener('submit', handleLoginSubmit);
 
 function handleLoginSubmit(event){
     event.preventDefault();
@@ -32,9 +58,6 @@ function handleLoginData(formData){
     })
 }
 
-logoutButton.addEventListener('click', () => {localStorage.removeItem('token')});
-
-
 novelForm.addEventListener('submit', handleNovelForm);
 
 function handleNovelForm(event){
@@ -48,5 +71,29 @@ function handleNovelForm(event){
         },
         body: formData
     })
-        .then(response => response.json())
+    .then(response => response.json())
+    .then(novelForm.reset())
 }
+
+graphicNovelForm.addEventListener('submit', handleGraphicNovelForm);
+
+function handleGraphicNovelForm(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target)
+    fetch(graphicNovelAPI, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem('token')}`,
+            "Accept": "application/json"
+        },
+        body: formData
+    })
+        .then(response => response.json())
+        .then(graphicNovelForm.reset())
+}
+
+logoutButton.addEventListener('click', () => {localStorage.removeItem('token')});
+
+fetch(graphicNovelAPI)
+    .then(response => response.json())
+    .then(result => console.log(result[0].summary))
