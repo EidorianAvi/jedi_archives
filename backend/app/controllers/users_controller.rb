5fff518451cd8305f3f0d16211a20867a327a0c3
EidorianAvi <reneavila1993@gmail.com>
2020-07-08 23:14:34 -0600

    Updat create user fetch for errors, start working on login error, start working on frontend design.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -11,11 +11,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(
-            username: params[:username],
-            password: params[:password]
-        )
-        render json: { user: @user}, status: :created
+     @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            render json: { user: @user }, status: :created
+        else
+            render json: { message: @user.errors.messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
     end
 
 end
--- frontend/app.js
+++ frontend/app.js
@@ -1,99 +1,150 @@
 const createUserForm = document.querySelector("#create-user-form");
-const loginForm = document.querySelector('#login-form');
-const novelForm = document.querySelector('#novel-form');
+const loginForm = document.querySelector("#login-form");
+const novelForm = document.querySelector("#novel-form");
 const graphicNovelForm = document.querySelector("#graphic-novel-form");
-const logoutButton = document.querySelector('#logout-button');
+const logoutButton = document.querySelector("#logout-button");
 const novelAPI = "http://localhost:3000/novels";
 const graphicNovelAPI = "http://localhost:3000/graphic_novels";
 const userAPI = "http://localhost:3000/users";
 
+// createUserForm.addEventListener('submit', handleCreateSubmit);
 
-createUserForm.addEventListener('submit', handleCreateSubmit);
-
-function handleCreateSubmit(event){
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    handleCreateData(formData);
+function handleCreateSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  handleCreateData(formData);
 }
 
-function handleCreateData(formData){
-    fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: formData.get('username'),
-            password: formData.get('password')
-        })
+function handleCreateData(formData) {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: formData.get("username"),
+      password: formData.get("password"),
+    }),
+  })
+    .then((response) => {
+      const notifier = document.querySelector("#notifier");
+      if (response.ok) {
+        notifier.className = "success";
+        notifier.textContent = "User Created";
+      } else {
+        notifier.className = "error";
+        notifier.textContent = "Username already taken";
+        throw new Error("Username already taken");
+      }
     })
-    .then(response => response.json())
-    .then(console.log)
+    .then(createUserForm.reset());
 }
 
-loginForm.addEventListener('submit', handleLoginSubmit);
+loginForm.addEventListener("submit", handleLoginSubmit);
 
-function handleLoginSubmit(event){
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    handleLoginData(formData);
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  handleLoginData(formData);
 }
 
-function handleLoginData(formData){
-    fetch("http://localhost:3000/login",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: formData.get('username'),
-            password: formData.get('password')
-        })
-    })
-    .then(response => response.json())
-    .then(response => {
-        const {token} = response
-        localStorage.setItem("token", token);
-        console.log(token)
-    })
+function handleLoginData(formData) {
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: formData.get("username"),
+      password: formData.get("password"),
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      const { token } = response;
+      localStorage.setItem("token", token);
+      let loginConfirmation = document.querySelector("#login-confirmation");
+      console.log(loginConfirmation);
+      loginConfirmation.className = "success";
+      loginConfirmation.innerText = "Welcome";
+    });
 }
 
-novelForm.addEventListener('submit', handleNovelForm);
+// novelForm.addEventListener('submit', handleNovelForm);
 
-function handleNovelForm(event){
-    event.preventDefault();
-    const formData = new FormData(event.target)
-    fetch(novelAPI,{
-        method:"POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem('token')}`,
-            "Accept": "application/json"
-        },
-        body: formData
-    })
-    .then(response => response.json())
-    .then(novelForm.reset())
+function handleNovelForm(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  fetch(novelAPI, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      Accept: "application/json",
+    },
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then(novelForm.reset());
 }
 
-graphicNovelForm.addEventListener('submit', handleGraphicNovelForm);
+// graphicNovelForm.addEventListener('submit', handleGraphicNovelForm);
 
 function handleGraphicNovelForm(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target)
-    fetch(graphicNovelAPI, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem('token')}`,
-            "Accept": "application/json"
-        },
-        body: formData
-    })
-        .then(response => response.json())
-        .then(graphicNovelForm.reset())
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  fetch(graphicNovelAPI, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      Accept: "application/json",
+    },
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then(graphicNovelForm.reset());
 }
 
-logoutButton.addEventListener('click', () => {localStorage.removeItem('token')});
+// logoutButton.addEventListener('click', () => {localStorage.removeItem('token')});
 
-fetch(graphicNovelAPI)
-    .then(response => response.json())
-    .then(result => console.log(result[0].summary))
+// const main = document.querySelector('#page');
+// const routes = {
+//     '/': HomePage,
+//     '/create_user': CreateUserPage,
+//     '/archive': ArchivePage
+// }
+
+// function HomePage(){
+//     return (`
+// <div id="login-box">
+// <form id="login-form">
+//     <input type="text" placeholder="Username" name="username" required/><br />
+//     <input type="password" placeholder="Password" name="password" required/><br />
+//     <input type="submit" value="Login"/><br/>
+//     <p id="login-confirmation></p>
+// </form>
+// </div>
+//     `)
+// }
+
+// function CreateUserPage(){
+// }
+
+// function ArchivePage(){
+// }
+
+// function router(event){
+//     const path = window.location.hash.split('#')[1] || "/";
+//     const page = routes[path];
+//     if(page){
+//         main.innerHTML = page();
+//     } else {
+//         main.innerHTML = `
+//             <section>
+//                 <h1>404 ERROR</h1>
+//             </section>
+//         `
+//     }
+// }
+
+// window.addEventListener('hashchange', router);
+// window.addEventListener('load', router);
